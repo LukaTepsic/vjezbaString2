c37b1ff445f94957d2c6d5f4c82441a696eda6ae
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-28 12:57:18 +0100

    Add files via upload

---
+++ vjezbaString2/vjezbaString2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s1 = "Hrvatska ima more";
+        string s2 = "Italija je prvak svijeta";
+
+        string s3 = s1.Substring(0, 8) + s2.Substring(6);
+
+        Console.WriteLine(s3);
+    }
+}
